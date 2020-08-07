6793709987aca67d661c3a411c3519c28d0ac799
L.D Libra <ldl@elementalcognition.com>
2020-08-07 17:40:35 -0400

    got font loading

--- portfolio/src/App.svelte
+++ portfolio/src/App.svelte
@@ -1,30 +1,67 @@
 <script>
-	export let name;
 </script>
 
+<header>
+	<h1>Recent Projects - 2020</h1>
+	<div class="contact-info">
+		<span>L.D Libra</span>
+		<span>writeme@ldlibra.com</span>
+		<span>218.821.1379</span>
+		<span>Brooklyn, NY</span>
+	</div>
+</header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<nav>
+		<div>
+			<div>Motion for Justice</div>
+			<div>April 4 - May 18</div>
+		</div>
+		<div>
+			<div>CPI</div>
+			<div>April 4 - May 18</div>
+		</div>
+		<div>
+			<div>ZeroPlace</div>
+			<div>April 4 - May 18</div>
+		</div>
+	</nav>
+	<section>
+		<section>
+			<div>
+				<h2>Client</h2>
+				<p></p>
+			</div>      
+			<div>
+				<h2>Team</h2>
+				<p></p>
+			</div>      
+			<div>
+				<h2>Summary</h2>
+				<p></p>
+			</div>      
+		</section>
+		<section>
+			<div>
+				<img />
+				<img />
+				<img />
+			</div>
+		</section>
+	</section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  header, main {
+    display: flex;
+    margin: 0 2em;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  header {
+    align-items: center;
+    justify-content: space-between;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .contact-info span {
+    margin-left: 0.5em;
+  }
 </style>
