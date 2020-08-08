cf4a0c795099ba0a254a2d053909767330e5b161
L.D Libra <ldl@elementalcognition.com>
2020-08-08 01:05:18 -0400

    start of layout and visuals

--- portfolio/src/App.svelte
+++ portfolio/src/App.svelte
@@ -2,13 +2,13 @@
 </script>
 
 <header>
-	<h1>Recent Projects - 2020</h1>
 	<div class="contact-info">
 		<span>L.D Libra</span>
 		<span>writeme@ldlibra.com</span>
 		<span>218.821.1379</span>
 		<span>Brooklyn, NY</span>
 	</div>
+	<h1>RECENT PROJECTS - 2020</h1>
 </header>
 <main>
 	<nav>
@@ -27,16 +27,20 @@
 	</nav>
 	<section>
 		<section>
-			<div>
-				<h2>Client</h2>
+			<div class="project-title-and-timespan">
+				<div>Motion for Justice</div>
+				<div>April 4 - May 18</div>
+			</div>
+			<div class="client">
+				<h2><sup>THE</sup>CLIENT</h2>
 				<p></p>
 			</div>      
-			<div>
-				<h2>Team</h2>
+			<div class="team">
+				<h2><sup>THE</sup>TEAM</h2>
 				<p></p>
 			</div>      
-			<div>
-				<h2>Summary</h2>
+			<div class="work">
+				<h2><sup>THE</sup>WORK</h2>
 				<p></p>
 			</div>      
 		</section>
@@ -51,17 +55,59 @@
 </main>
 
 <style>
-  header, main {
+  header, main, .contact-info {
     display: flex;
-    margin: 0 2em;
   }
 
   header {
-    align-items: center;
-    justify-content: space-between;
-  }
+		flex-direction: column;
+		justify-content: space-between;
+		margin-bottom: 2em;
+	}
+	
+	header h1 {
+		font-size: 1.7em;
+		margin-bottom: 0.2em;
+		border-bottom: solid 1em lightsteelblue;
+	}
 
-  .contact-info span {
-    margin-left: 0.5em;
-  }
+  .contact-info {
+		display: flex;
+		flex-direction: column;
+		text-transform: uppercase;
+		letter-spacing: 0.15em;
+		border-right: solid 0.5em #ffa6b4;
+	}
+	
+	sup {
+		font-size: 0.4em;
+	}
+
+	nav {
+		display: none;
+	}
+
+	section {
+		width: 100%;
+	}
+
+	.project-title-and-timespan {
+		display: flex;
+		border-left: solid 0.5em #5fffa5;
+		flex-direction: column;
+		align-items: flex-end;
+		width: calc(100% - 0.5em);
+	}
+
+	.client {
+		border-right: solid 4em #ff7656;
+	}
+
+	.team {
+		border-right: solid 10em #b6f8ff;
+	}
+
+	.work {
+		border-right: solid 6.5em fuchsia;
+	}
 </style>
